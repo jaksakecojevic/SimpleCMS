bc62eb0456b33839c1774b879add7bb35a50b5ba
Rar9 <RKroells@web.de>
2023-04-19 04:27:58 +0200

    More Componets

--- env.ts
+++ env.ts
@@ -28,26 +28,26 @@ let env: {
 	LANGUAGE: 'en', // defines default language
 	TRANSLATIONS: { en: 'English', de: 'German' },
 
-	SYSTEMLANGUAGE: 'de', // defines default System language
+	SYSTEMLANGUAGE: 'en', // defines default System language
 
 	//for express
 	HOST: 'http://localhost',
 	PORT: 5173,
 
 	// YOUR API_KEYs
-	MAPBOX_API_TOKEN: '',
-	GOOGLE_API_TOKEN: '',
+	MAPBOX_API_TOKEN: 'pk.eyJ1Ijoicmtyb2VsbHMiLCJhIjoiY2xiaDAxdXJjMDl3MTNvbm14ZGhibjhvbiJ9.nx2Xokl7u_4lwf_VhEiNUQ',
+	GOOGLE_API_TOKEN: 'AIzaSyA0_ru-cUxE6dBKJrANnSwWeCTbLU2DDhY',
 	YOUTUBE_API_KEY: '',
 	VIMEO_API_KEY: '',
 	TWITCH_API_KEY: '',
 	TIKTOK_API_KEY: '',
 
 	// for mongodb......for Atlas use DB_HOST:"mongodb+srv://XXXX",
-	DB_URL: 'xxxx:27017',
-	DB_NAME: 'test',
-	DB_USER: 'celestialme',
-	DB_PASSWORD: 'lEnH8TAthqjbMQpe',
-	DB_HOST: 'mongodb+srv://main.qfd1u.mongodb.net',
+	DB_URL: '172.17.0.1:27017',
+	DB_NAME: 'NewSimpleCMS',
+	DB_USER: 'admin',
+	DB_PASSWORD: 'Getin1972!',
+	DB_HOST: 'mongodb://172.17.0.1:27017',
 
 	API: '',
 	PKG: {
@@ -58,17 +58,3 @@ let env: {
 env.API = `${env.HOST}:${env.PORT}/api`;
 
 export default env;
-
-// import pkg from './package.json' assert { type: 'json' };
-// let env = {
-// 	HOST: 'http://localhost',
-// 	PORT: 5173,
-// 	DB_NAME: 'test',
-// 	DB_USER: 'celestialme',
-// 	DB_PASSWORD: 'lEnH8TAthqjbMQpe',
-// 	DB_HOST: 'mongodb+srv://main.qfd1u.mongodb.net',
-// 	API: '',
-// 	PKG: { VERSION: pkg.version }
-// };
-// env.API = `${env.HOST}:${env.PORT}/api`;
-// export default env;
